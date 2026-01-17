13f0e0b5a448284504b407d6a24359e0907ebda8
Hai Thai <hai_thai@cable.comcast.com>
2026-01-17 00:17:48 -0500

    Upgrade to the latest npm pkgs

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,6 +1,6 @@
 import { Navigate } from 'react-router-dom';
 import { isAuthenticated } from '../utils/auth';
 
-export default function PrivateRoute({ children }: { children: JSX.Element }) {
+export default function PrivateRoute({ children }: { children: React.ReactElement }) {
   return isAuthenticated() ? children : <Navigate to="/" replace />;
 }
